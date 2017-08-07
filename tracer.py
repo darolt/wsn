3545fb4e5f4e6c3d3a1bd1ee89bea34463728f82
Jean Da Rolt <jeandarolt@gmail.com>
2017-08-06 23:16:18 -0300

    moving files to respective source folder

--- tracer.py
+++
@@ -1,9 +0,0 @@
-"""Utility class used to store local traces."""
-class Tracer(dict):
-  alive_nodes_label = 'Number of alive nodes'
-  rounds_label      = 'Rounds'
-  energies_label    = 'Energy (J)'
-
-  def __init__(self):
-    self['alive_nodes'] = (self.alive_nodes_label, self.rounds_label, [])
-    #self['energies']    = (self.energies_label,    self.rounds_label, [])
