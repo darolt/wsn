6020b58d8429b818f86b435287f38097001dcb10
Jean Da Rolt <jeandarolt@gmail.com>
2017-08-06 23:18:37 -0300

    Improving simulation procedure & setup build

--- config.py
+++ config.py
@@ -1,23 +1,87 @@
 import math
 
+## Runtime configuration
+MAX_ROUNDS = 15000
+# number of transmissions of sensed information to cluster heads or to
+# base station (per round)
+MAX_TX_PER_ROUND = 1
+
+NOTIFY_POSITION = 0
+
+# Describe the scenarios that will be simulated
+# scenarios should be described in the following format:
+# scenario_name = (routing_topology, sleep_scheduling)
+# where routing_topology may be:
+#   'DC'   : Direct Communication
+#   'MTE'  : Minimum Transmission Energy
+#   'LEACH': LEACH
+#   'FCM  ': Fuzzy C-Means
+# and sleep_scheduling may be:
+#   None                 : No sleep scheduling (mind that None is not a string)
+#   'Pso'                : Particle Swarm Optimization
+#   'ModifiedPso'        : Modified PSO
+#   'GeneticAlgorithm'   : Genetic Algorithm
+# and aggregation_model may be:
+#   'zero'  : Zero cost
+#   'total' : 100% cost
+#   'linear': TODO spec
+#   'log'   : log cost
+
+scenario_0 = ('DC',    None,         'zero')
+scenario_1 = ('LEACH', None,         'zero')
+scenario_2 = ('MTE',   None,         'total')
+scenario_3 = ('FCM',   None,         'zero')
+scenario_4 = ('FCM',  'ModifiedPso', 'zero')
+scenario_5 = ('FCM',  'Pso',         'zero')
+# list with all scenarios to simulate
+scenarios = [
+              #scenario_0,
+              #scenario_1,
+              #scenario_2,
+              #scenario_3,
+              "cf.FITNESS_ALPHA=0.5",
+              "cf.FITNESS_BETA=0.5",
+              scenario_4,
+              scenario_5,
+              "cf.FITNESS_ALPHA=1.0",
+              "cf.FITNESS_BETA=0.0",
+              scenario_4,
+              scenario_5,
+              "cf.FITNESS_ALPHA=0.0",
+              "cf.FITNESS_BETA=1.0",
+              scenario_4,
+              scenario_5
+            ]
+
+## tracer options
+TRACE_ENERGY         = 0
+TRACE_ALIVE_NODES    = 1
+TRACE_COVERAGE       = 1
+TRACE_LEARNING_CURVE = 0
+
+
+## Network configurations:
 # number of nodes
 NB_NODES = 300
-
-MAX_ROUNDS = 15000
+# node sensor range
+COVERAGE_RADIUS    = 10 # meters 
+# node transmission range
+TX_RANGE = 30 # meters
+BSID = -1
 # area definition
 AREA_WIDTH = 250.0
 AREA_LENGTH = 250.0
 # base station position
 BS_POS_X = 125.0
-BS_POS_Y = -75.0
+BS_POS_Y = 125.0
 # packet configs
-MSG_LENGTH = 4000
-HEADER_LENGTH = 150
-# number of transmissions of sensed information to cluster heads or to
-# base station (per round)
-MAX_TX_PER_ROUND = 1
+MSG_LENGTH = 4000 # bits
+HEADER_LENGTH = 150 # bits
+# initial energy at every node's battery
+INITIAL_ENERGY = 2 # Joules
 
-# ENERGY MODEL
+
+## Energy Configurations
 # energy dissipated at the transceiver electronic (/bit)
 E_ELEC = 50e-9 # Joules
 # energy dissipated at the data aggregation (/bit)
@@ -29,37 +93,29 @@ E_MP = 0.0013e-12 # Joules
 # free-space channel (/bin/m^2)
 E_FS = 10e-12 # Joules
 THRESHOLD_DIST = math.sqrt(E_FS/E_MP) # meters
-BSID = -1
-LEACH_NB_CLUSTERS = 5
-# initial energy at every node's battery
-INITIAL_ENERGY = 2 # Joules
-# fuzzyness coeficient
+
+
+## Routing configurations:
+NB_CLUSTERS = 5
+# FCM fuzzyness coeficient
 FUZZY_M = 2
 
+
+## Sleep Scheduling configurations:
+NB_INDIVIDUALS   = 20
+MAX_ITERATIONS = 100
+# ALPHA and BETA are the fitness function' weights
+# where ALPHA optimizes energy lifetime, BETA the coverage
+FITNESS_ALPHA  = 0.5
+FITNESS_BETA   = 0.5
+WMAX = 0.6
+WMIN = 0.1
+
+
+## Other configurations:
+# GRID
+GRID_PRECISION = 0.1 # in meters
 # useful constants (for readability)
 INFINITY = float('inf')
 MINUS_INFINITY = float('-inf')
 
-# runtime configuration
-RUN_DC    = 0
-RUN_MTE   = 1
-RUN_LEACH = 1
-RUN_FCM   = 1
-RUN_PSO   = 0
-
-# PSO sleep scheduling configuration
-PSO_E = 0.5
-PSO_F = 0.5
-PSO_NB_PARTICLES   = 30
-COVERAGE_RADIUS    = 20 # meters 
-# ALPHA, BETA and GAMMA are the fitness function' weights
-# where ALPHA optimizes energy lifetime, BETA the coverage and GAMMA reduces the overlapping
-PSO_FITNESS_ALPHA  = 1
-PSO_FITNESS_BETA   = 0
-PSO_FITNESS_GAMMA  = 0
-PSO_MAX_ITERATIONS = 50
-PSO_WMAX = 0.9
-PSO_WMIN = 0.4
-
-# GRID
-GRID_PRECISION = 0.1 # in meters
--- setup.py
+++ setup.py
@@ -6,15 +6,36 @@
 from distutils.core import setup, Extension
 import os
 
+# this project uses c++11 codes
 os.environ["CC"] = "g++-5 -std=c++11"
 
-pso = Extension('_pso',
-                sources=['pso_wrap.cxx', 'pso.cpp', 'regions.cpp'])
+swig_opts   = ['-shadow', '-c++']
+source_path = 'cc/'
+build_path  = 'build/temp.linux-x86_64-2.7/'+source_path
 
-setup (name        = 'pso',
-       version     = '0.1',
-       author      = "JRO",
+modified_pso      = Extension('cc._modified_pso',
+                              sources=[source_path+'modified_pso.cc',
+                                       source_path+'regions.cc',
+                                       source_path+'optimizer.cc',
+                                       source_path+'modified_pso.i'],
+                              swig_opts=swig_opts)
+pso               = Extension('cc._pso',
+                              sources=[source_path+'pso.cc',
+                                       source_path+'pso.i'],
+                              extra_objects=[build_path+'regions.o',
+                                             build_path+'optimizer.o'],
+                              swig_opts=swig_opts)
+genetic_algorithm = Extension('cc._genetic_algorithm',
+                              sources=[source_path+'genetic_algorithm.cc',
+                                       source_path+'genetic_algorithm.i'],
+                              extra_objects=[build_path+'regions.o',
+                                             build_path+'optimizer.o'],
+                              swig_opts=swig_opts)
+# compile C++ libraries
+setup (name        = 'Optimizers',
+       version     = '1.0',
+       author      = "Jean Da Rolt",
        description = """C++ wrappers for performance optimization.""",
-       ext_modules = [pso],
-       py_modules  = ["pso"],
+       ext_modules = [modified_pso, pso, genetic_algorithm],
+       py_modules  = ["modified_pso", "pso", "genetic_algorithm"],
        )
