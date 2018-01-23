66b7fe1c49ed513828ba266ca02c0364237ecbc9
Jean Da Rolt <jeandarolt@gmail.com>
2018-01-23 11:09:03 -0200

    Removing tmp files

--- bckup/0711/tracer.py
+++
@@ -1,9 +0,0 @@
-"""Utility class used to store local traces."""
-class Tracer(dict):
-  alive_nodes_label = 'Number of alive nodes'
-  rounds_label      = 'Rounds'
-  energies_label    = 'Energy (J)'
-
-  def __init__(self):
-    self['alive_nodes'] = (self.alive_nodes_label, self.rounds_label, [])
-#    self.energies    = (energies_label, rounds_label, [])
