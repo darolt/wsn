c066fcd40b47e64cb42dd2c6dbcc9e1c2bb9f2ad
Jean Da Rolt <jeandarolt@gmail.com>
2018-01-23 13:31:24 -0200

    Fixing raise syntax

--- python/routing/dijkstra.py
+++ python/routing/dijkstra.py
@@ -47,7 +47,7 @@ def Dijkstra(G,start,end=None):
       vwLength = D[v] + G[v][w]
       if w in D:
         if vwLength < D[w]:
-          raise ValueError, "Dijkstra: found better path to already-final vertex"
+          raise ValueError("Dijkstra: found better path to already-final vertex")
       elif w not in Q or vwLength < Q[w]:
         Q[w] = vwLength
         P[w] = v
